a52c7cda81a46d9e60737d76eab6920a075b78b3
SangWon7242 <gotkdqja@gmail.com>
2025-09-06 09:47:02 +0900

    Answer 엔티티에 question 변수를 ManyToOne으로 변경

--- src/main/java/com/sbs/qnaService/boundedContext/answer/entity/Answer.java
+++ src/main/java/com/sbs/qnaService/boundedContext/answer/entity/Answer.java
@@ -22,5 +22,7 @@ public class Answer {
 
   private LocalDateTime createDate;
 
-//  private Question question;
+  // 하나의 질문의 답변이 여러개일 수 있다.
+  @ManyToOne // 좌측은 많고 우측은 하나다.
+  private Question question;
 }
