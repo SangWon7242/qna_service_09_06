ad61d200d0e8bdb0368c2e0367e4a8b539d63093
SangWon7242 <gotkdqja@gmail.com>
2025-09-06 11:50:41 +0900

    각 테스트 케이스의 독립성 확보

--- src/main/java/com/sbs/qnaService/boundedContext/question/repository/QuestionRepository.java
+++ src/main/java/com/sbs/qnaService/boundedContext/question/repository/QuestionRepository.java
@@ -1,7 +1,10 @@
 package com.sbs.qnaService.boundedContext.question.repository;
 
 import com.sbs.qnaService.boundedContext.question.entity.Question;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +14,9 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
   Question findBySubjectAndContent(String subject, String content);
 
   List<Question> findBySubjectLike(String keyword);
+
+  @Modifying
+  @Transactional
+  @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
+  void clearAutoIncrement();
 }
--- src/test/java/com/sbs/qnaService/QnaServiceApplicationTests.java
+++ src/test/java/com/sbs/qnaService/QnaServiceApplicationTests.java
@@ -25,18 +25,39 @@ class QnaServiceApplicationTests {
   @Autowired
   private QuestionRepository questionRepository;
 
-	@Test
-  @DisplayName("데이터 저장")
-	void t1() {
+  @BeforeEach // 테스트 케이스 실행 전에 한번 실행
+  void beforeEach() {
+    // 질문 데이터 삭제
+    questionRepository.deleteAll();
+    questionRepository.clearAutoIncrement();
+    
     Question q1 = new Question();
     q1.setSubject("sbb가 무엇인가요?");
     q1.setContent("sbb에 대해서 알고 싶습니다.");
     q1.setCreateDate(LocalDateTime.now());
-    questionRepository.save(q1);  // 첫번째 질문 저장
+    questionRepository.save(q1);
 
     Question q2 = Question.builder()
         .subject("스프링부트 모델 질문입니다.")
         .content("id는 자동으로 생성되나요?")
+        .createDate(LocalDateTime.now())
+        .build();
+
+    questionRepository.save(q2);
+  }
+
+	@Test
+  @DisplayName("데이터 저장")
+	void t1() {
+    Question q1 = new Question();
+    q1.setSubject("스프링부트 학습은 어떻게 해야 하나요?");
+    q1.setContent("스프링부트 학습은 처음입니다.");
+    q1.setCreateDate(LocalDateTime.now());
+    questionRepository.save(q1);  // 첫번째 질문 저장
+
+    Question q2 = Question.builder()
+        .subject("자바를 이용한 객체지향 설계는 어떻게 하나요?")
+        .content("자바 객체지향 설계 방법을 모르겠습니다.")
         .createDate(LocalDateTime.now())
         .build();
 
