0de2084ba060500eb6bffd9e75ad4d940cf47579
SangWon7242 <gotkdqja@gmail.com>
2025-09-07 11:22:53 +0900

    루트 경로 접속시 질문 리스트로 리다이렉트

--- src/main/java/com/sbs/qnaService/boundedContext/home/controller/HomeController.java
+++ src/main/java/com/sbs/qnaService/boundedContext/home/controller/HomeController.java
@@ -2,15 +2,11 @@ package com.sbs.qnaService.boundedContext.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/home")
 public class HomeController {
-  @GetMapping("/main")
-  @ResponseBody
+  @GetMapping("/")
   public String showMain() {
-    return "안녕";
+    return "redirect:/question/list";
   }
 }
