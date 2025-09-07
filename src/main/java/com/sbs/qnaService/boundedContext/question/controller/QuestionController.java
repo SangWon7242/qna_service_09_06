d293e62d832224c2f3e4fe4bf94a732206c33dab
SangWon7242 <gotkdqja@gmail.com>
2025-09-07 10:50:12 +0900

    질문 리스트 구현

--- src/main/java/com/sbs/qnaService/boundedContext/question/controller/QuestionController.java
+++ src/main/java/com/sbs/qnaService/boundedContext/question/controller/QuestionController.java
@@ -1,17 +1,29 @@
 package com.sbs.qnaService.boundedContext.question.controller;
 
+import com.sbs.qnaService.boundedContext.question.entity.Question;
+import com.sbs.qnaService.boundedContext.question.repository.QuestionRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/question")
+@RequiredArgsConstructor
 public class QuestionController {
+  private final QuestionRepository questionRepository;
+
   @GetMapping("/list")
   public String showList(Model model) {
-    model.addAttribute("age", 20);
+    // findAll() : 모든 데이터를 조회
+    // SELECT * FROM question;
+    List<Question> questionList = questionRepository.findAll();
+    model.addAttribute("questionList", questionList);
 
     return "question/question_list";
   }
