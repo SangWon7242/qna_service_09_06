662a7f57bb7f3ad62dec5ca9253ddd2d0b268569
SangWon7242 <gotkdqja@gmail.com>
2025-09-07 10:43:38 +0900

    Model 객체를 이용해서 템플릿에 데이터 전달

--- src/main/java/com/sbs/qnaService/boundedContext/question/controller/QuestionController.java
+++ src/main/java/com/sbs/qnaService/boundedContext/question/controller/QuestionController.java
@@ -1,6 +1,8 @@
 package com.sbs.qnaService.boundedContext.question.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/question")
 public class QuestionController {
   @GetMapping("/list")
-  public String showList() {
+  public String showList(Model model) {
+    model.addAttribute("age", 20);
+
     return "question/question_list";
   }
 }
