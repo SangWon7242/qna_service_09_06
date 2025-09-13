ae4313f008d7bc7cbf9628ded8df024002ce26fc
SangWon7242 <gotkdqja@gmail.com>
2025-09-13 09:18:35 +0900

    질문 상세 컨트롤러 도입

--- src/main/java/com/sbs/qnaService/boundedContext/question/controller/QuestionController.java
+++ src/main/java/com/sbs/qnaService/boundedContext/question/controller/QuestionController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -26,5 +27,10 @@ public class QuestionController {
     model.addAttribute("questionList", questionList);
 
     return "question/question_list";
+  }
+
+  @GetMapping("/detail/{id}")
+  public String detail(Model model, @PathVariable("id") Integer id) {
+    return "question/question_detail";
   }
 }
