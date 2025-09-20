30505d6bf0da5a412b6db2c492438696f1447ffd
SangWon7242 <gotkdqja@gmail.com>
2025-09-20 10:11:22 +0900

    중복 회원 가입 방지하기

--- src/main/java/com/sbs/qnaService/boundedContext/user/controller/UserController.java
+++ src/main/java/com/sbs/qnaService/boundedContext/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.sbs.qnaService.boundedContext.user.input.UserCreateForm;
 import com.sbs.qnaService.boundedContext.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,18 +25,30 @@ public class UserController {
 
   @PostMapping("/signup")
   public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+    String form = "user/signup_form";
+
     if (bindingResult.hasErrors()) {
-      return "user/signup_form";
+      return form;
     }
 
     if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
       bindingResult.rejectValue("password2", "passwordInCorrect",
           "2개의 패스워드가 일치하지 않습니다.");
-      return "user/signup_form";
+      return form;
     }
 
-    userService.create(userCreateForm.getUsername(),
-        userCreateForm.getEmail(), userCreateForm.getPassword1());
+    try {
+      userService.create(userCreateForm.getUsername(),
+          userCreateForm.getEmail(), userCreateForm.getPassword1());
+    } catch (DataIntegrityViolationException e) { // DB에 무결성 제약조건이 위반되었을 때 발생하는 예외
+      e.printStackTrace();
+      bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+      return form;
+    } catch (Exception e) {
+      e.printStackTrace();
+      bindingResult.reject("signupFailed", e.getMessage());
+      return form;
+    }
 
     return "redirect:/";
   }
